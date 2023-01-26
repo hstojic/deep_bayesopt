85b17599228c56db6ad48a33b2f183ec53de5b7d
william cowley <william.cowley01@gmail.com>
2023-01-26 17:13:26 +0000

    dummy commit

--- uanets/models/layers.py
+++ uanets/models/layers.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Contains some custom uanet layers
+Contains some custom uanet layers.
 """
 import tensorflow as tf
 from tensorflow_probability import distributions as tfpd
